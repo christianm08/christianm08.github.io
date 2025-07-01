5439a086d39370995c973e9e067e745ea6a5f762
christianm08 <164552309+christianm08@users.noreply.github.com>
2025-07-01 17:51:04 +0200

    update

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
     console.log('Index page loaded');
 });
+
+// Funzione per navigare senza .html nell'URL
+function goPage(page) {
+    let url = window.location.origin + '/';
+    fetch(url + page, {method: 'HEAD'}).then(resp => {
+        if (resp.ok) {
+            window.location.href = url + page;
+        } else {
+            window.location.href = url + page + '.html';
+        }
+    }).catch(() => {
+        window.location.href = url + page + '.html';
+    });
+}
