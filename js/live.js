5a7cdda4c193f937fc1c1a8c78d6ccef4d26df22
christianm08 <164552309+christianm08@users.noreply.github.com>
2025-07-01 17:43:13 +0200

    update

--- js/live.js
+++ js/live.js
@@ -45,3 +45,16 @@ function goHome() {
         window.location.href = url;
     });
 }
+// Funzione per navigare senza .html nell'URL
+function goPage(page) {
+    let url = window.location.origin + '/';
+    fetch(url + page, {method: 'HEAD'}).then(resp => {
+        if (resp.ok) {
+            window.location.href = url + page;
+        } else {
+            window.location.href = url + page + '.html';
+        }
+    }).catch(() => {
+        window.location.href = url + page + '.html';
+    });
+}
--- js/streaming.js
+++ js/streaming.js
@@ -527,3 +527,17 @@ function showMyList() {
     });
 }
 
+// Funzione per navigare senza .html nell'URL
+function goPage(page) {
+    let url = window.location.origin + '/';
+    fetch(url + page, {method: 'HEAD'}).then(resp => {
+        if (resp.ok) {
+            window.location.href = url + page;
+        } else {
+            window.location.href = url + page + '.html';
+        }
+    }).catch(() => {
+        window.location.href = url + page + '.html';
+    });
+}
+
